b1cd983102f7033d539f82a26a946aebc5cbf518
Kyle Barisone <kbarisone001@student.butte.edu>
2017-01-26 22:38:34 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,27 @@
+/* 1) Create a program so that the computer picks a random number (x) ranging 
+from 0 to 20. 
+
+2) Make the computer desplay every number on the screen from 0 to 20 in 
+their own seperate box. 
+
+3) The numbers listed above must be in numerical order starting 
+from 0 and ending at 20. 
+
+4) If the user clicks on the correct number, Theuser wins and a message displays
+saying "congratulations!". 
+
+5) If the incorrect number is guessed by the user, a notification pops up saying 
+whether the number that was clicked is higher or lower than the number chosen
+by the random number generator in step one.
+
+6) If the random number is greater than the number chosen by the user, all 
+numbers that are less than the number chosen by the user are eliminated and no 
+longer show up on the screen.
+
+7) If the random number is less than the number chosen by the user, all numbers
+that are greater than the number chosen by the user are eliminated and no 
+longer show up on the screen.
+
+8) Repeat steps 6 and 7 until the correct number is clicked on by the user,
+prompting the congratulations! screen to appear. */
+
