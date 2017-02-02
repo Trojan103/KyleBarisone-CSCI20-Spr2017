5d1256a0286a7332e87ab62c6e3982cb13c3fde3
Kyle Barisone <kbarisone001@student.butte.edu>
2017-02-02 23:03:05 +0000

    Lab1.3 submission

--- lab13/lab13.cpp
+++ lab13/lab13.cpp
@@ -7,11 +7,13 @@
      of population increase since the year 1. Making this program able to
      estimate future as well as past populations */
      
-    int currentPop = 324473020;
-    int currentYear = 2017;
-    int estPopulation = currentPop/currentYear;
+    int currentPop = 324473133;
+    int secondsPerYear = 31536000;
+    int babiesPerYear = secondsPerYear / 8; 
+    int deathsPerYear = secondsPerYear / 11;
+    int immigrantsPerYear = secondsPerYear / 29;
     int year = 1;
-    int prediction = year * estPopulation;
+    int prediction =  currentPop + (babiesPerYear - deathsPerYear + immigrantsPerYear);
     
     cout << "Enter the desired year to estimate the population";
     cin >> year;
