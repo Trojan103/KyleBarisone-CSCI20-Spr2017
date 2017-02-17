5d3b7ce90678797fd6d2f0d701e4c54bc27087d5
Kyle Barisone <kbarisone001@student.butte.edu>
2017-02-17 00:15:38 +0000

    Assignment 1.0 submission

---
+++ Project/Assign.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+ #include <string>
+ using namespace std;
+ /* This program will calculate your weekly salary based on the numbers inputted into the code. It is calculated with a 17% tax as well as 130 
+ dollars of healthcare each week.*/
+ 
+ //This struct holds the variables for salary, hours worked, and name
+ struct paySlip {
+     double salaryPerHour;
+     double hoursWorked;
+     string name;
+     string last;
+ };
+ 
+ int main(){
+     
+     //This is where the user imputs their information into the machine.
+    paySlip  paySlip1;
+    cout << "This program will calculate how much you will be paid on your pay slip." << endl;
+    
+    cout << "First things first, What is your first name?" << endl;
+    cin >> paySlip1.name;
+    
+    cout << "And your last name?" <<endl;
+    cin >> paySlip1.last;
+    
+    cout << "Please Enter the amount of money you make per hour." << endl;
+    cin >> paySlip1.salaryPerHour;
+    
+    cout << "Now enter the amount of hours you have worked." << endl;
+    cin >> paySlip1.hoursWorked;
+    
+    //These are my calculations for figuring out the total after tax
+    double weeklySalary = 7 * paySlip1.hoursWorked * paySlip1.salaryPerHour;
+    double healthCareCharge = weeklySalary - 130;
+    double netProfit = healthCareCharge - (healthCareCharge * .17);
+    
+    //This is where the code is outputted to the user with the final amounts.
+    cout << "Weekly wages for: " << endl;
+    cout << "Name: " << paySlip1.last << ", " << paySlip1.name << endl;
+    cout << "Hours Worked: " << paySlip1.hoursWorked << endl;
+    cout << "gross pay: $" << weeklySalary << endl;
+    cout << "Net Profit after Health Care Charges and 17% tax: $" << netProfit <<endl;
+    
+ }
