3de7cce5d3751a469ca81229d0ec2d03ee135a6b
Kyle Barisone <kbarisone001@student.butte.edu>
2017-05-02 20:03:56 +0000

    lab46 submission

--- lab46/lab46.cpp
+++ lab46/lab46.cpp
@@ -8,15 +8,28 @@ ifstream fin;
 ifstream fout;
 
 string name;
+string name1;
+string name2;
 int i;
 int semester;
+int semester1;
+int semester2;
 int units[i];
+int units1[i];
+int units2[i];
 char letterGrade[i];
+char letterGrade1[i];
+char letterGrade2[i];
 int totalUnits;
+int totalUnits1;
+int totalUnits2;
 double GPA;
+double GPA1;
+double GPA2;
 
 
 fin.open("info.txt");
+fout.open("info.txt");
 getline(fin,name);
 fin >> semester;
 
@@ -28,8 +41,31 @@ fin >> letterGrade[i];
 fin >> totalUnits;
 fin >> GPA;
 
+getline(fin,name1);
+fin >> semester1;
+
+for(int i = 0; i < 4; i++){
+fin >> units1[i];
+fin >> letterGrade1[i];
+}
+
+fin >> totalUnits1;
+fin >> GPA1;
+
+getline(fin,name2);
+fin >> semester2;
+
+for(int i = 0; i < 4; i++){
+fin >> units2[i];
+fin >> letterGrade2[i];
+}
+
+fin >> totalUnits2;
+fin >> GPA2;
+
 
 fin.close();
+fout.close();
 
 cout <<"Name:             " << name << endl;
 cout <<"Current Semester: " << semester << endl;
@@ -40,6 +76,26 @@ for (int i=0; i<4; i++){
 
 cout <<"Total Units:      " << totalUnits << endl;
 cout <<"GPA:              " << GPA << endl;
+
+cout <<"Name:             " << name1 << endl;
+cout <<"Current Semester: " << semester1 << endl;
+
+for (int i=0; i<4; i++){
+    cout << "Units: " << units1[i] << " Grade:   " << letterGrade1[i] << endl;
+}  
+
+cout <<"Total Units:      " << totalUnits1 << endl;
+cout <<"GPA:              " << GPA1 << endl;
+
+cout <<"Name:             " << name2 << endl;
+cout <<"Current Semester: " << semester2 << endl;
+
+for (int i=0; i<4; i++){
+    cout << "Units: " << units2[i] << " Grade:   " << letterGrade2[i] << endl;
+}  
+
+cout <<"Total Units:      " << totalUnits2 << endl;
+cout <<"GPA:              " << GPA2 << endl;
     
   
     
