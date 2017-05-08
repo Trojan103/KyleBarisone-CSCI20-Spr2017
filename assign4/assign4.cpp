1947e65ca857216648709c96b3a750e201f8cf0d
Kyle Barisone <kbarisone001@student.butte.edu>
2017-05-08 00:00:16 +0000

    Assignment 4.0 submission

---
+++ assign4/assign4.cpp
@@ -0,0 +1,174 @@
+/*
+ Kyle Barisone
+ may 7 2017
+ This program uses 4 total files. This main file is used to comparte the answers from answers.txt to the student's answers in studentAnswers.txt. It also keeps
+ track of the student score based on the amount he or she gets wrong and or omits. After the answers are compared it outputs the grade to grade.txt and tells the students name as 
+ well as what questions he or she missed as well as the total score.
+ */
+
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+//Class for calculating grade
+
+class finalGrade {
+    public: 
+    
+        void answerChecker(string answerkey[21], int incorrect[21], string studentAnswers[23],  int i, int j, int k, double score){
+            
+        // This program takes in the first and last name of a user before it starts comparing grades from the two files so it does not impact final score
+        
+           for (i = 0; i < 21; i++) {
+                j = i + 2; 
+        
+        //if statement that adds 1 to score if the student is correct
+        
+        if (answerkey[i] == studentAnswers[j]) 
+                score = score + 1.0; 
+        
+        else if (answerkey[i] != studentAnswers[j]) { // if statement for incorrect answer
+        
+            // If student omits a question 1 point is taken off
+            
+            if (studentAnswers[j] == " ") { 
+                score = score - 1;
+                incorrect[k] = i; 
+                k++;
+            }
+            
+            // If answer is incorrect .25 points are deducted
+            
+            else {  
+                score = score - 0.25;
+                
+                //stores incorrect question number
+                incorrect[k] = i; 
+                k++;
+            }
+        }
+    }
+    
+    
+    totalScore = score; 
+    NumOfMistake = k;  
+}
+        // keeps the total score of student
+        
+        
+        //keeps track of the number of answers wrong
+        
+        int GetnumWrong() const {
+        return NumOfMistake;
+        } 
+        
+        
+        double GetScore() const {
+        return totalScore;
+        } 
+        
+        
+        
+        
+    private:
+    
+        double totalScore = 0.0; 
+        int NumOfMistake = 0; 
+};
+
+// class functions
+
+int main() {
+    
+    //initializing file input and output
+    ifstream fin; 
+    ifstream fin1; 
+    ofstream fout; 
+    
+    //intitializing variables and arrays.
+    string answerkey[21]; 
+    string studentAnswers[23];
+    string incorrect1[21];
+    double score = 0.0; 
+    int incorrect[21];
+    
+    //variables for arrays 
+    int i = 0; 
+    int j = 0; 
+    int k = 0; 
+    
+    //initializes class
+    finalGrade grade; 
+    
+    cout << "Loading submitted answers" << endl;
+    
+    // this opens the two text files for comparison
+    fin.open("answers.txt");
+    
+    //if the file is not present the application will close with warning message.
+    if (!fin.is_open()) { 
+        cout << endl;
+        cout << "Missing file: 'answers.txt...'" << endl;
+        return 1;
+    }
+    cout << endl;
+    cout << "reading answers" << endl;
+    
+    fin1.open("studentAnswers.txt");
+    
+    //if the file is not present the application will close
+    if (!fin1.is_open()) { 
+        cout << endl;
+        cout << "Missing file: 'studentAnswer.txt...'" << endl;
+        return 1;
+    }
+    cout << endl;
+    cout << "Recording answers in the grade book." << endl;
+    
+    //opens file to tell student their final grade
+    fout.open("grade.txt");
+    if (!fout.is_open()) {
+      cout << endl;
+      cout << "Missing file: 'studentResult.txt...'" << endl;
+      return 1;
+    }
+    
+    cout << endl;
+    
+    //reads answer key data
+    for (i = 0; i < 18; i++) {
+        fin >> answerkey[i];
+    }
+    
+    // takes the input from student answers to be analyzed
+    for (i = 0; i < 20; i++) {
+        fin1 >> studentAnswers[i];
+    }
+    
+    
+    // this class compares the array and determines score
+    grade.answerChecker(answerkey, incorrect, studentAnswers,  i, j, k, score);
+    
+    fout << endl;
+    fout << "Student Name: " << studentAnswers[0] << " " << studentAnswers[1] << endl; // studentAnswer.txt contains student's name, so the sentence output the name to text file.
+    fout << endl;
+    
+    // for loop for showing question number of which answers were incorrect
+    for (i = 0; i < grade.GetnumWrong(); i++) {
+        fout << "You got #" << incorrect[i] + 1 << "." << " incorrect. (-1.25)" << endl; // element of single array starts form 0, so I add 1 for output.
+    }
+        //Tells the user their total score
+        fout << endl;
+        fout << "Total Score: " << grade.GetScore() << endl; // output total score to text file
+        fout << endl;
+    
+    //closes out of the program
+    cout << endl;
+    cout << "Logging out." << endl;
+    fin.close();
+    fin1.close();
+    fout.close();
+    
+    return 0;
+}
