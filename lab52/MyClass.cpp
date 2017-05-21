d51bf0f04ccb7c3d72d0e05cea6cff8628c2d308
Kyle Barisone <kbarisone001@student.butte.edu>
2017-05-21 17:55:34 +0000

    lab52 submission

---
+++ lab52/MyClass.cpp
@@ -0,0 +1,16 @@
+
+#include "MyClass.h"
+
+MyClass::MyClass() : num_(0) {}
+
+void MyClass::Output() {
+  cout << "My number is: " << num_ << endl;
+}
+
+void MyClass::SetNumber(int num){
+   num_ = num;
+}
+
+int MyClass::GetNumber(){
+   return num_;
+}
